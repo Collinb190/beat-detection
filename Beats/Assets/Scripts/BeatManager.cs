f8114f4d914363fa91be16ac0fe41bb92fefe4d5
Lin <100497279+Collinb190@users.noreply.github.com>
2025-03-16 12:25:31 -0500

    Multiple Beats Down

--- Beats/Assets/Scripts/BeatManager.cs
+++ Beats/Assets/Scripts/BeatManager.cs
@@ -1,7 +1,5 @@
-using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Events;
-using UnityEngine.VFX;
 
 public class BeatManager : MonoBehaviour
 {
@@ -13,6 +11,12 @@ public class BeatManager : MonoBehaviour
     {
         foreach (Beats beat in _beats)
         {
+            /*
+             * sampledTime represents the current position in the audio, measured in samples.
+             * _audioSource.clip.frequency * beatLength gives you the number of samples per beat.
+             * _audioSource.timeSamples gives you the exact position (in samples) of where the audio is currently playing.
+             * (_audioSource.timeSamples / numberOfSamplesPerBeat) gives you the current beat's position in the audio.
+             */
             float sampledTime = (_audioSource.timeSamples / (_audioSource.clip.frequency * beat.GetBeatLength(_bpm)));
             beat.CheckForNewBeat(sampledTime);
         }
@@ -22,20 +26,20 @@ public class BeatManager : MonoBehaviour
 [System.Serializable]
 public class Beats
 {
-    [SerializeField] private float _interval;
+    [SerializeField] private float _magnifier; // Adjust the rate of the beat: 1 is normal, 0.5 half speed, 2 is double speed
     [SerializeField] private UnityEvent _trigger;
     private int _lastBeat;
 
-    public float GetBeatLength(float bpm)
+    public float GetBeatLength(float bpm) // determines the amount of beats happening each second
     {
-        return 60f / (bpm * _interval);
+        return 60f / (bpm * _magnifier);
     }
 
-    public void CheckForNewBeat (float beat)
+    public void CheckForNewBeat(float currentbeat)
     {
-        if (Mathf.FloorToInt(beat) != _lastBeat)
+        if (Mathf.FloorToInt(currentbeat) != _lastBeat)
         {
-            _lastBeat = Mathf.FloorToInt(beat);
+            _lastBeat = Mathf.FloorToInt(currentbeat);
             _trigger.Invoke();
         }
     }
