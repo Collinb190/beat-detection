e70ea5d76374707d3c8eb49ff32d899899ae5982
Lin <100497279+Collinb190@users.noreply.github.com>
2025-03-18 07:10:52 -0500

    Scene environment done; do UI now

--- Beats/Assets/Scripts/BeatScroller.cs
+++ Beats/Assets/Scripts/BeatScroller.cs
@@ -12,6 +12,6 @@ public class BeatScroller : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.right * beatTempo * Time.deltaTime);
+        transform.Translate(Vector3.left * beatTempo * Time.deltaTime);
     }
 }
