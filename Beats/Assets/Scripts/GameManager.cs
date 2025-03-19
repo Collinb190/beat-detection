a8ab1e11cdda93933ba80dfb28e1fd38a5210f50
Lin <100497279+Collinb190@users.noreply.github.com>
2025-03-19 10:06:52 -0500

    game loop works; polish it up now

--- Beats/Assets/Scripts/GameManager.cs
+++ Beats/Assets/Scripts/GameManager.cs
@@ -1,8 +1,12 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
     [SerializeField] private AudioSource gameAudio; // Reference to your AudioSource
+    [SerializeField] private GameObject gameOverUI; // Reference to the UI panel
+    [SerializeField] private GameObject arrowSpawner;
+    [SerializeField] private GameObject target;
 
     void Update()
     {
@@ -11,7 +15,17 @@ public class GameManager : MonoBehaviour
 
     void EndGame()
     {
-        Debug.Log("Game Over! Audio Finished.");
-        Time.timeScale = 0;
+        gameOverUI.SetActive(true); // Show the UI when the audio stops
+        arrowSpawner.SetActive(false);
+        target.SetActive(false);
+    }
+    public void QuitGame()
+    {
+        Debug.Log("Stopped the game");
+        Application.Quit();
+    }
+    public void MainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
     }
 }
